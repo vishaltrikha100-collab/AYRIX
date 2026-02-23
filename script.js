33c02d8a5444c0261052e3c2b740fd6cd14188bd
vishaltrikha100-collab <vishaltrikha100@gmail.com>
2026-02-23 06:48:04 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("AYRIX website loaded successfully");
+const text = "Because every little beginning deserves gentle care through the first days, weeks, and moments that matter most.";
+const speed = 45;
+
+let index = 0;
+const target = document.getElementById("typing-text");
+
+function typeEffect() {
+  if (index < text.length) {
+    target.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, speed);
+  }
+}
+
+window.addEventListener("load", typeEffect);
+
